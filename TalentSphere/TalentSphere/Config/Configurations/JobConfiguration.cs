53cffc695990d2a4cfffb8263763583e31dec120
LNU <2478552@cognizant.com>
2026-03-16 11:09:53 +0530

    .

--- TalentSphere/TalentSphere/Config/Configurations/JobConfiguration.cs
+++ TalentSphere/TalentSphere/Config/Configurations/JobConfiguration.cs
@@ -18,7 +18,6 @@ namespace TalentSphere.Config.Configurations
             builder.Property(j => j.Requirements).HasMaxLength(2000);
 
             builder.Property(j => j.Status)
-                    .HasConversion<string>()
                    .HasDefaultValue(JobStatus.Open);
         }
     }
